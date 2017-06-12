54febdd8bc33a57d83314419baa2af19b2add233
Tom Watson <tom.james.watson@googlemail.com>
2017-06-12 12:40:43 +0200

    turn off no-shadow

--- index.js
+++ index.js
@@ -58,7 +58,6 @@ module.exports = {
     // Variables
     'no-catch-shadow': 2,
     'no-label-var': 2,
-    'no-shadow': 2,
     'no-shadow-restricted-names': 2,
     'no-undef-init': 2,
     'no-use-before-define': 2,
